69081f66d40e7dd37eb50b9b4f53457fd6d177ec
rjweaver <rjweaver@me.com>
2013-12-03 16:51:30 -0800

    added facebook authentication for username

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,16 +10,17 @@ var app = angular.module("myapp", ["firebase"]);
 
 app.config(function($routeProvider) {
     $routeProvider.
-        when('/', {controller:chatController, templateUrl:'views/chat.html'}).
+        when('/', {controller:'chatController', templateUrl:'views/chat.html'}).
         otherwise({redirectTo:'/'});
 });
 
 
-function chatController($scope, angularFire, $location, $anchorScroll){
-    var ref = new Firebase("https://dat-chat1.firebaseio.com/");
+app.controller('chatController',['$scope','angularFire','$location', '$anchorScroll','angularFireAuth', function($scope, angularFire, $location, $anchorScroll, angularFireAuth){
+    var ref = new Firebase("https://dat-chat1.firebaseio.com/messages");
+    var username = null;
     var query = ref.limit(30);
 
-    $scope.messages = [];
+    //$scope.messages = [];
     angularFire(ref, $scope, "messages"); //binding the model
 
     ref.on('child_added', function(snap){
@@ -44,10 +45,72 @@ function chatController($scope, angularFire, $location, $anchorScroll){
 
     $scope.addMessage = function(e) {
         if (e.keyCode != 13) return;
-        $scope.messages.push({from: $scope.name, body: $scope.msg});
+        $scope.messages.push({from: username, body: $scope.msg});
         $scope.msg = "";
 
         $scope.stayBottom();
     }
 
-}
+
+    //Authentication
+    angularFireAuth.initialize(ref, {scope: $scope, name: "user", path: '/'});
+    $scope.login = function(){
+        angularFireAuth.login("facebook");
+    }
+
+    $scope.logout = function() {
+        angularFireAuth.logout();
+    }
+
+    $scope.$on("angularFireAuth:login", function(evt, user) {
+        // User logged in.
+        console.log('Logged In: ' + user.name );
+        username = user.name;
+    });
+    $scope.$on("angularFireAuth:logout", function(evt) {
+        // User logged out.
+        console.log('Logged Out');
+        username = '';
+    });
+    $scope.$on("angularFireAuth:error", function(evt, err) {
+        // There was an error during authentication.
+        console.log('error' + err + ", " + evt);
+    });
+
+
+
+
+}]);
+
+/*
+
+app.controller('fullController',['$scope','angularFireAuth', function($scope, angularFireAuth){
+    var ref = new Firebase("https://dat-chat1.firebaseio.com/messages");
+    //var myUserID = null;
+    angularFireAuth.initialize(ref, {scope: $scope, name: "user", path: '/'});
+
+    $scope.login = function(){
+        angularFireAuth.login("facebook");
+    }
+
+    $scope.logout = function() {
+        angularFireAuth.logout();
+    }
+
+    $scope.$on("angularFireAuth:login", function(evt, user) {
+        // User logged in.
+        console.log('Logged In: ' + user.name );
+        username = user.name;
+    });
+    $scope.$on("angularFireAuth:logout", function(evt) {
+        // User logged out.
+        console.log('Logged Out');
+        username = '';
+    });
+    $scope.$on("angularFireAuth:error", function(evt, err) {
+        // There was an error during authentication.
+        console.log('error' + err + ", " + evt);
+    });
+
+}]);
+*/
